b3f376894ca253d932ef785f2c3a802019ee2864
Vikas Sharma <vkassharma19492@gmail.com>
2023-05-27 03:29:05 +0530

    terminal like website

--- topcoin/src/Terminal/index.js
+++ topcoin/src/Terminal/index.js
@@ -3,31 +3,48 @@ import './terminal.css';
 export const Terminal = () => {
     return (
         <div className="terminal">
-            <div className="terminal__line"><strong>Introducing TOP COIN:</strong> Breaking Barriers to Financial Freedom</div>
+            <div className="terminal__line"><strong>Introducing <span style={{ color: 'green' }}>TOP COIN</span>:</strong> Breaking Barriers to Financial Freedom</div><br />
             <div className="terminal__prompt">
-                <div className="terminal__prompt__label">Welcome to the exciting world of TOP COIN:</div>
-                <div className="terminal__prompt__input">the cryptocurrency that's here to revolutionize the way we think about ownership. In a bold and unconventional move, the founders of TOP COIN have renounced all ownership rights, putting the market in control of its destiny. With TOP COIN, we believe in the power of the people to shape their own financial future.</div>
-            </div>
+                <div className="terminal__prompt__label">TopCoin<span style={{ color: 'yellow' }}>:</span><span style={{ color: 'white' }}><strong>~</strong></span><span style={{ color: 'red' }}><strong>$</strong></span></div>
+                <div className="terminal__prompt__input">Welcome to the exciting world of TOP COIN, the cryptocurrency that's here to revolutionize the way we think about ownership. In a bold and unconventional move, the founders of TOP COIN have renounced all ownership rights, putting the market in control of its destiny. With TOP COIN, we believe in the power of the people to shape their own financial future.</div>
+            </div><br/>
             <div className="terminal__prompt">
-                <div className="terminal__prompt__label">The Idea Behind TOP COIN:</div>
+                <div className="terminal__prompt__label1">The Idea Behind TOP COIN:</div>
                 <div className="terminal__prompt__input">TOP COIN was born out of a deep desire to challenge the traditional notions of ownership and create a level playing field for everyone, including ourselves—the admittedly broke founders. We understand the struggles and limitations faced by individuals who are striving for financial freedom. Hence, we set out on a mission to develop a cryptocurrency that not only addresses those challenges but also serves as a beacon of hope for those in need.</div>
-            </div>
+            </div><br/>
             <div className="terminal__prompt">
-                <div className="terminal__prompt__label">Why Choose TOP COIN?:</div>
+                <div className="terminal__prompt__label1">Why Choose TOP COIN?:</div>
                 <div className="terminal__prompt__input">Financial Freedom for All: At TOP COIN, we firmly believe that everyone deserves a chance to break free from the shackles of financial constraints. By renouncing ownership and putting the power in the hands of the market, we aim to create a truly decentralized and inclusive financial ecosystem.</div>
-            </div>
+            </div><br/>
             <div className="terminal__prompt">
-                <div className="terminal__prompt__label">Bitcoin-Like Simplicity:</div>
+                <div className="terminal__prompt__label1">Bitcoin-Like Simplicity:</div>
                 <div className="terminal__prompt__input">Inspired by the revolutionary impact of Bitcoin, TOP COIN is designed to be user-friendly and accessible to all. With TOP COIN, you can seamlessly make payments, purchase goods, and engage in transactions, just like with any other major cryptocurrency.</div>
-            </div>
+            </div><br/>
             <div className="terminal__prompt">
-                <div className="terminal__prompt__label">TopCoin:</div>
-                <div className="terminal__prompt__input">Some Text</div>
-            </div>
+                <div className="terminal__prompt__label1">Empowering the Community:</div>
+                <div className="terminal__prompt__input">As founders, we are proudly transparent about our financial situation. By utilizing TOP COIN, we aspire to empower individuals who are struggling with money, helping them become active participants in the world of digital currencies.</div>
+            </div><br/>
             <div className="terminal__prompt">
-                <div className="terminal__prompt__label">TopCoin:</div>
-                <div className="terminal__prompt__input">Some Text</div>
+                <div className="terminal__prompt__label1">Built on Binance Smart Chain:</div>
+                <div className="terminal__prompt__input">TOP COIN leverages the robust infrastructure of the Binance Smart Chain, ensuring fast and secure transactions while benefiting from the extensive support and compatibility of the Binance ecosystem.</div>
+            </div><br />
+            <div className="terminal__prompt">
+                <div className="terminal__prompt__label2">Join the TOP COIN Movement:</div>
+                <div className="terminal__prompt__input1">Are you ready to break free from the chains of financial limitations? Join us on this extraordinary journey as we strive to empower individuals from all walks of life. Together, we can build a future where financial freedom is within reach for everyone.</div>
+                <div className="terminal__prompt__input1">While our pockets may be empty, our hearts are filled with passion, dedication, and an unwavering belief in the potential of TOP COIN. Join us on GitHub and be a part of this remarkable movement. Together, we can create a world where financial freedom knows no bounds!</div>
+            </div><br />
+            <div className="terminal__prompt">
+                <div className="terminal__prompt__label3">Meet the Visionaries:</div>
+                <div className="terminal__prompt__input">Makoto and Vairo, the Broke Founders</div>
+                <div className="terminal__prompt__input2">In the thrilling realm of cryptocurrencies, where dreams are forged and boundaries are shattered, we introduce you to the visionary duo behind TOP COIN: Makoto and Vairo, the audacious and relentless founders. Prepare to embark on a journey that encapsulates their unwavering determination to break the shackles of financial limitations.</div>
+                <div className="terminal__prompt__input3">Makoto, whose name means "truth" in Japanese, personifies unwavering honesty and authenticity. With a fiery spirit and an unyielding belief in the power of change, Makoto envisions a future where financial freedom is within reach for all.</div>
+                <div className="terminal__prompt__input3">Vairo, a name derived from the word "vairocana," representing the supreme Buddha of light, symbolizes enlightenment and illumination. With an incisive intellect and an insatiable thirst for knowledge, Vairo guides the technical aspects of TOP COIN, shedding light on the path toward a decentralized and inclusive financial landscape.</div>
+                <div className="terminal__prompt__input2">Bound by their shared experiences of financial struggles, Makoto and Vairo channel their passion and determination to defy conventional norms. Embracing the power of community support and armed with laptops and boundless ambition, they embarked on an extraordinary journey to breathe life into TOP COIN.</div>
+                <div className="terminal__prompt__input1">Together, Makoto and Vairo form an unstoppable force—an alliance of truth and enlightenment. They envision a world where financial freedom transcends barriers, empowering individuals to achieve their dreams without limitations.</div>
+                <div className="terminal__prompt__input2">So, fellow dreamers and seekers of financial liberation, join Makoto and Vairo on this exhilarating adventure. Let's rally behind these audacious founders as they pave the way toward a future where financial barriers crumble. Together, we'll unlock the doors to a world where freedom and prosperity flourish with TOP COIN!</div>
+                <div className="terminal__prompt__input1">"Within the realm of abundance, every soul is entitled to not just one chance, but an infinite tapestry of opportunities. Embrace the boundless possibilities that life unfolds, for within the depths of goodwill and abundance, dreams find wings to soar and hearts find the resilience to persist."</div>
             </div>
+            <div className="terminal__line"><strong> - Makoto and Vairo</strong></div>
         </div>
     );
 };
