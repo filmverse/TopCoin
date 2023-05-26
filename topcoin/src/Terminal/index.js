81180585e44cdef0f350c1cc886c508e67981663
Vikas Sharma <vkassharma19492@gmail.com>
2023-05-27 01:52:04 +0530

    terminal like website

---
+++ topcoin/src/Terminal/index.js
@@ -0,0 +1,33 @@
+import './terminal.css';
+
+export const Terminal = () => {
+    return (
+        <div className="terminal">
+            <div className="terminal__line"><strong>Introducing TOP COIN:</strong> Breaking Barriers to Financial Freedom</div>
+            <div className="terminal__prompt">
+                <div className="terminal__prompt__label">Welcome to the exciting world of TOP COIN:</div>
+                <div className="terminal__prompt__input">the cryptocurrency that's here to revolutionize the way we think about ownership. In a bold and unconventional move, the founders of TOP COIN have renounced all ownership rights, putting the market in control of its destiny. With TOP COIN, we believe in the power of the people to shape their own financial future.</div>
+            </div>
+            <div className="terminal__prompt">
+                <div className="terminal__prompt__label">The Idea Behind TOP COIN:</div>
+                <div className="terminal__prompt__input">TOP COIN was born out of a deep desire to challenge the traditional notions of ownership and create a level playing field for everyone, including ourselves—the admittedly broke founders. We understand the struggles and limitations faced by individuals who are striving for financial freedom. Hence, we set out on a mission to develop a cryptocurrency that not only addresses those challenges but also serves as a beacon of hope for those in need.</div>
+            </div>
+            <div className="terminal__prompt">
+                <div className="terminal__prompt__label">Why Choose TOP COIN?:</div>
+                <div className="terminal__prompt__input">Financial Freedom for All: At TOP COIN, we firmly believe that everyone deserves a chance to break free from the shackles of financial constraints. By renouncing ownership and putting the power in the hands of the market, we aim to create a truly decentralized and inclusive financial ecosystem.</div>
+            </div>
+            <div className="terminal__prompt">
+                <div className="terminal__prompt__label">Bitcoin-Like Simplicity:</div>
+                <div className="terminal__prompt__input">Inspired by the revolutionary impact of Bitcoin, TOP COIN is designed to be user-friendly and accessible to all. With TOP COIN, you can seamlessly make payments, purchase goods, and engage in transactions, just like with any other major cryptocurrency.</div>
+            </div>
+            <div className="terminal__prompt">
+                <div className="terminal__prompt__label">TopCoin:</div>
+                <div className="terminal__prompt__input">Some Text</div>
+            </div>
+            <div className="terminal__prompt">
+                <div className="terminal__prompt__label">TopCoin:</div>
+                <div className="terminal__prompt__input">Some Text</div>
+            </div>
+        </div>
+    );
+};
